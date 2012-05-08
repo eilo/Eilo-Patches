c92a16cb03b063987467493cea2a892cd8c042ff
Eilo <eilo2518@gmail.com>
2012-05-08 00:59:16 -0500

    borrando archivos demas, que no se que hacen aqui.

--- sql/updates/world/traducciones/game_tele_rean.sql
+++
@@ -1,5 +0,0 @@
--- Cambio de nombre de 2 sitios que se usa mucho por el staff
--- Eilo (https://github.com/eilo)
-
-UPDATE game_tele SET NAME='test' WHERE id=817;
-UPDATE game_tele SET NAME='icc' WHERE id=1418;
